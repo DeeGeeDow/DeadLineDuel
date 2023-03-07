c4effeacdd0cb43d999b6596e671dae21914b565
Muhammad Fahmi Irfan <66018985+DeeGeeDow@users.noreply.github.com>
2023-03-07 22:12:19 +0700

    fix : gamepad bug

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -6,16 +6,32 @@ using UnityEngine.InputSystem;
 public class PlayerManager : MonoBehaviour
 {
     public GameObject playerPrefab;
+    private PlayerInput Player1, Player2;
     void Awake()
     {
-        PlayerInput.Instantiate(playerPrefab,
-            controlScheme: "Keyboard1", pairWithDevices: new InputDevice[] { Keyboard.current, Gamepad.current }).gameObject
+        var gamepadCount = Gamepad.all.Count;
+        if (gamepadCount >= 2)
+        {
+            Player1 = PlayerInput.Instantiate(playerPrefab, controlScheme: "Gamepad", pairWithDevice: Gamepad.all[0]);
+            Player2 = PlayerInput.Instantiate(playerPrefab, controlScheme: "Gamepad", pairWithDevice: Gamepad.all[1]);
+        }
+        else if (gamepadCount == 1)
+        {
+            Player1 = PlayerInput.Instantiate(playerPrefab, controlScheme: "Gamepad", pairWithDevice: Gamepad.all[0]);
+            Player2 = PlayerInput.Instantiate(playerPrefab, controlScheme: "Keyboard2", pairWithDevice: Keyboard.current);
+        }
+        else
+        {
+            Player1 = PlayerInput.Instantiate(playerPrefab, controlScheme: "Keyboard1", pairWithDevice: Keyboard.current);
+            Player2 = PlayerInput.Instantiate(playerPrefab, controlScheme: "Keyboard2", pairWithDevice: Keyboard.current);
+        }
+
+        Player1.gameObject
             .GetComponent<PlayerController>()
             .setIsPlayer1(true)
             .setPosition(new Vector3(-5, 0, 0));
 
-        PlayerInput.Instantiate(playerPrefab,
-            controlScheme: "Keyboard2", pairWithDevice: Keyboard.current).gameObject
+        Player2.gameObject
             .GetComponent<PlayerController>()
             .setIsPlayer1(false)
             .setPosition(new Vector3(5, 0, 0));
